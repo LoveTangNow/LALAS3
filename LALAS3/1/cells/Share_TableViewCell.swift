19a24697fb20d39be529496c857630c6d0ddd334
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-15 21:44:33 +0800

    分享 cell

---
+++ LALAS3/1/cells/Share_TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  Share_TableViewCell.swift
+//  LALAS3
+//
+//  Created by Thomas Liu on 16/9/15.
+//  Copyright © 2016年 ThomasLiu. All rights reserved.
+//
+
+import UIKit
+
+class Share_TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var UIImageView_Main: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
