8ea7d53a93e37db5738f28068f3ffa0d4b3f702e
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-15 21:47:42 +0800

    登录 Controller

--- LALAS3/Main/Login_ViewController.swift
+++ LALAS3/Main/Login_ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class Login_ViewController: UIViewController {
+    
+    @IBOutlet weak var UITextField_Zhanghao: UITextField!
+    @IBOutlet weak var UITextField_Mima: UITextField!
+    @IBOutlet weak var UITextField_Yanzhengma: UITextField!
+    
+    @IBOutlet weak var UIButton_ChangeYanzhengma: UIButton!
+    @IBOutlet weak var UIButton_Main: UIButton!
+    @IBOutlet weak var UIButton_ZhuCe: UIButton!
+    
+    @IBAction func ChangeYanZhengMa_Click(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func Main_Click(_ sender: AnyObject) {
+    }
+    
+    @IBAction func Jump_Click(_ sender: AnyObject) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        UITextField_Zhanghao.placeholder = "手机号或者邮箱"
+        UITextField_Mima.placeholder = "密码"
+        UITextField_Yanzhengma.placeholder = "验证码"
+        
+        UIButton_ChangeYanzhengma.setTitle("", for: UIControlState.normal)
+        UIButton_Main.setTitle("登录", for:  UIControlState.normal)
+        UIButton_ZhuCe.setTitle("没有账号？点这里", for:  UIControlState.normal)
+        
         // Do any additional setup after loading the view.
     }
 
