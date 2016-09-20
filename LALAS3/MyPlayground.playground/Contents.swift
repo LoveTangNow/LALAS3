bc699616cd3ce205756fd219d28634b1ffbf63a7
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-21 01:50:39 +0800

    add playground

---
+++ LALAS3/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a  = 2.5
+var b  = 2.6
+var c  = 2.4
+
+var d  = Int(a)
+
+ d  = Int(b)
+
+ d  = Int(c)
+
+
+
+
+
