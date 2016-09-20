1d96d38f96c8293ad8f15180c89926e2619499bf
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-20 13:31:19 +0800

    PCC cell 添加动作 数目的增减

--- LALAS3/Main/Cell/CommentsPraiseDemote_TableViewCell.swift
+++ LALAS3/Main/Cell/CommentsPraiseDemote_TableViewCell.swift
@@ -14,13 +14,54 @@ class CommentsPraiseDemote_TableViewCell: UITableViewCell {
     @IBOutlet weak var UIButton_2: UIButton!
     @IBOutlet weak var UIButton_3: UIButton!
     
+    var Pinglun_NUM = 0
+    var Zan_NUM = 0
+    var Cai_NUM = 0
+    
+    var Pinglun_BOOL = false
+    var Zan_BOOL = false
+    var Cai_BOOL = false
+    
+    var Sender_ID:String = ""
+
+    
     @IBAction func UIButton_1_Click(_ sender: AnyObject) {
+        if Pinglun_BOOL == false {
+            Pinglun_NUM += 1
+            Pinglun_BOOL = true
+        } else {
+            Pinglun_NUM -= 1
+            Pinglun_BOOL = false
+        }
+        
+        UIButton_1.setTitle("评论" + String(Pinglun_NUM), for:.normal)
         
     }
     @IBAction func UIButton_2_Click(_ sender: AnyObject) {
+        //赞
+        if Zan_BOOL == false {
+            Zan_NUM += 1
+            Zan_BOOL = true
+        } else {
+            Zan_NUM -= 1
+            Zan_BOOL = false
+        }
+        
+        UIButton_2.setTitle("赞" + String(Zan_NUM), for:.normal)
         
     }
     @IBAction func UIButton_3_Click(_ sender: AnyObject) {
+        //踩
+        if Cai_BOOL == false {
+            Cai_NUM += 1
+            Cai_BOOL = true
+        } else {
+            Cai_NUM -= 1
+            Cai_BOOL = false
+        }
+        
+        UIButton_3.setTitle("踩" + String(Cai_NUM), for:.normal)
+
         
     }
 
@@ -31,6 +72,10 @@ class CommentsPraiseDemote_TableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        
+         UIButton_1.setTitle("评论" + String(Cai_NUM), for:.normal)
+         UIButton_2.setTitle("赞" + String(Cai_NUM), for:.normal)
+         UIButton_3.setTitle("踩" + String(Cai_NUM), for:.normal)
 
         // Configure the view for the selected state
     }
