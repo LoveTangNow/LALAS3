1ea888db865909372120b581cbe0e3aef48da7b9
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-21 01:50:17 +0800

    try core data

---
+++ LALAS3/LALAS3-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  LALAS3-Bridging-Header.h.h
+//  LALAS3
+//
+//  Created by Thomas Liu on 16/9/20.
+//  Copyright © 2016年 ThomasLiu. All rights reserved.
+//
+
+#ifndef LALAS3_Bridging_Header_h
+#define LALAS3_Bridging_Header_h
+
+
+#endif /* LALAS3_Bridging_Header_h */
+
+#import "sqlite3.h"
---
+++ LALAS3/RR+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  RR+CoreDataClass.swift
+//  
+//
+//  Created by Thomas Liu on 16/9/20.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class RR: NSManagedObject {
+
+}
