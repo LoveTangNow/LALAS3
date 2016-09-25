65efe964efc38becf28e148f2811d37ae917692b
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-25 22:14:59 +0800

    动态详情页 重新用新的空间2构建

--- LALAS3/1/MessiageDetail_TableViewController.swift
+++ LALAS3/1/MessiageDetail_TableViewController.swift
@@ -27,15 +27,49 @@ class MessiageDetail_TableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "OnePhoto_H_TableViewCell", for: indexPath) as! OnePhoto_H_TableViewCell
+        let image1 = #imageLiteral(resourceName: "Black")
+        let image2 = #imageLiteral(resourceName: "White")
         
-        cell.UIImageView_Main.image = Imageload_Black
         
-        return cell
+        
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "News_Information_TableViewCell", for: indexPath) as! News_Information_TableViewCell
+                
+                cell.UIImageViewUserIcon.image = image1
+                cell.UIImageViewSetting.image = image1
+                cell.UIImageViewVip_.image = image1
+                cell.UIImageViewV_.image = image2
+                
+                cell.UILabelDetail.text = "hehe"
+
+                return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "OnePhoto_H_NTableViewCell", for: indexPath) as! OnePhoto_H_NTableViewCell
+                
+                cell.UIImageView_M.image = image1
+                
+                return cell
+            }
+            
+        }
+        else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Pinglun_NTableViewCell", for: indexPath) as! Pinglun_NTableViewCell
+ 
+            cell.UIImageView_Icon.image = image1
+            
+            return cell
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
     }
 
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
