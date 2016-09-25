419cd429373dbc06683c2cf8c749e6c2bfae2356
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-25 22:18:05 +0800

    测试viewcontrollor

---
+++ LALAS3/Shit/TViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TViewController.swift
+//  LALAS3
+//
+//  Created by Thomas Liu on 2016/9/25.
+//  Copyright © 2016年 ThomasLiu. All rights reserved.
+//
+
+import UIKit
+
+class TViewController: UIViewController {
+
+    @IBOutlet weak var UIPickerView_M: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
