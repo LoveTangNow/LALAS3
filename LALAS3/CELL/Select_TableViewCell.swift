a56dfcf17ed6d87acde21b225b319340ab9551a5
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-27 17:57:50 +0800

    性别那种有几种选择的 cell

---
+++ LALAS3/CELL/Select_TableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  Select_TableViewCell.swift
+//  LALAS3
+//
+//  Created by Thomas Liu on 2016/9/27.
+//  Copyright © 2016年 ThomasLiu. All rights reserved.
+//
+
+import UIKit
+
+class Select_TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var UILabel_M: UILabel!
+    @IBOutlet weak var UISegmentedControl_M: UISegmentedControl!
+    
+    @IBAction func UISegmentedControl_Valuechanged(_ sender: AnyObject) {
+        let a  = UISegmentedControl_M.titleForSegment(at: UISegmentedControl_M.selectedSegmentIndex)! as String
+        
+        print(a)
+        UserDefaults.standard.set(a,forKey:"Sex")
+        UserDefaults.standard.synchronize()
+    }
+    
+    var NumberOfSegments = 0
+    
+    
+    override func awakeFromNib() {
+        
+        NumberOfSegments = 1
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
