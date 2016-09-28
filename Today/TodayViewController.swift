ab30e227b5a851129969b06aebea05f002fdf97f
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-29 00:15:00 +0800

    增加扩展 语言 发消息的 photopicker
    扩展：action：用 Safari 打开之类的。。。表情包。。。Today
    语言：无意中添加的，不知道怎么用 以后研究
    尝试把发消息的 photo 单独布置 不在 tablevewi 里了

---
+++ Today/TodayViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TodayViewController.swift
+//  Today
+//
+//  Created by Thomas Liu on 2016/9/28.
+//  Copyright © 2016年 ThomasLiu. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
